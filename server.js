168c03fc63c4ce4caedfd7bc66fe6053a819921d
kalyan-kashaboina <kalyan.k@aifalabs.com>
2025-05-30 20:10:31 +0530

    Updated Frontend Url Cors

--- server.js
+++ server.js
@@ -9,7 +9,21 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+// Restrict CORS to your frontend only
+const allowedOrigins = [process.env.FRONTEND_URL];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
