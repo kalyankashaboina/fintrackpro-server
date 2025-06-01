b67274906a410cfe16684295560f17f00c8b4f08
kalyankashaboina <kalyankashaboina07@gmail.com>
2025-06-01 13:44:19 +0530

    Added dev url also

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 // Restrict CORS to your frontend only
-const allowedOrigins = ['https://fintrackpro-three.vercel.app'];
+const allowedOrigins = ['https://fintrackpro-three.vercel.app', 'http://localhost:8080' ];
 
 
 app.use(cors({
